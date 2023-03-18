1649981d88a91efb68357d826d5ad58e77b64b84
vivek singh <103558318+VIV3072@users.noreply.github.com>
2023-03-18 22:58:21 +0530

    Time: 32 ms (14.30%), Space: 47.3 MB (17.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<HashMap<Character,Integer>,List<String>> map=new HashMap<>();
-        for(String s: strs){
-            HashMap<Character,Integer> mp=new HashMap<>();
-            for(int i=0;i<s.length();i++){
-                 mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        HashMap<String,ArrayList<String>> map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String key=genratekey(strs[i]);
+            
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
             }
-             if(!map.containsKey(mp))
-                 map.put(mp,new ArrayList<>());
-            map.get(mp).add(s);
-             
+            map.get(key).add(strs[i]);
         }
-         return new ArrayList<>(map.values());
+        List<List<String>> ans=new ArrayList<>();
+        for(String key:map.keySet()){
+            ans.add(map.get(key));
+        }
+        return ans;
+        
+        
+        
+    }
+    public static String genratekey(String s){
+        int[] fre=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            fre[ch-'a']=fre[ch-'a']+1;
+            
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<fre.length;i++){
+            sb.append(fre[i]+" ");
+        }
+        return sb.toString();
         
     }
 }
